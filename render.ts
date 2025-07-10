cfe999798bc8d38b2a751e8b18cd5db920bd334e
SidneyMalapit <smala6@uic.edu>
2025-07-09 19:02:14 -0500

    mapping route to view

--- render.ts
+++ render.ts
@@ -3,6 +3,17 @@ import Handlebars from 'handlebars';
 
 const mainTemplate = Handlebars.compile(fs.readFileSync('views/templates/main.hbs', { encoding: 'utf8' }));
 
+export const views = fs.readdirSync('views', { encoding: 'utf8' })
+  .filter((name) => name.endsWith('.hbs'))
+  .map((name) => name.slice(0, -4));
+
+const partials = fs.readdirSync('views/partials', { encoding: 'utf8' });
+
+for (const partial of partials) {
+  if (!partial.endsWith('.hbs')) { continue; }
+  Handlebars.registerPartial(partial.slice(0, -4), getPartial(partial));
+}
+
 export function compileView(name: string, template = mainTemplate, ctx = {}) {
   return template({
     body: getView(name),
@@ -12,4 +23,8 @@ export function compileView(name: string, template = mainTemplate, ctx = {}) {
 
 export function getView(name: string) {
   return fs.readFileSync(`views/${name}.hbs`, { encoding: 'utf8' });
+}
+
+function getPartial(name: string) {
+  return fs.readFileSync(`views/partials/${name}`, { encoding: 'utf8' });
 }
--- router.ts
+++ router.ts
@@ -1,11 +1,17 @@
-import { compileView } from './render';
 import { default as express, Router } from 'express';
+import { compileView, views } from './render';
 
 const router = Router();
 
 router.use(express.static('public'));
 
 router.get('/', (_, res) => res.redirect('/home'));
+
+router.use((req, res, next) => {
+  const name = req.path.slice(1);
+  if (!views.includes(name)) { return next(); }
+  res.send(compileView(name));
+});
 
 router.use((_, res) => {
   res.status(404).send(compileView('.not-found'));
